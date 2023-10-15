1682ccc7a518c6e806894230731a4a47eb72e178
jjungyeun <wjyddd@naver.com>
2023-10-15 22:42:46 +0900

    feat #2 - RecruitmentController 구현
    - RecruitmentController에 6가지 API 구현
    - 채용공고 등록 API
    - 채용공고 수정 API
    - 채용공고 삭제 API
    - 채용공고 리스트 검색 API
    - 채용공고 상세정보 조회 API
    - 채용공고 지원 API
    - 필요 DTO 구현

---
+++ src/main/java/com/wjy/wantedpreonboarding/controller/RecruitmentController.java
@@ -0,0 +1,73 @@
+package com.wjy.wantedpreonboarding.controller;
+
+import com.wjy.wantedpreonboarding.dto.req.RecruitmentApplyDto;
+import com.wjy.wantedpreonboarding.dto.req.RecruitmentCreateDto;
+import com.wjy.wantedpreonboarding.dto.req.RecruitmentEditDto;
+import com.wjy.wantedpreonboarding.dto.res.RecruitmentCreateResponseDto;
+import com.wjy.wantedpreonboarding.dto.res.RecruitmentDetailResponseDto;
+import com.wjy.wantedpreonboarding.dto.res.RecruitmentSearchResponseDto;
+import com.wjy.wantedpreonboarding.service.RecruitmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/recruitments")
+@RequiredArgsConstructor
+public class RecruitmentController {
+
+    private final RecruitmentService recruitmentService;
+
+    @PostMapping
+    public ResponseEntity<RecruitmentCreateResponseDto> create(
+            @RequestBody RecruitmentCreateDto createDto
+    ) {
+        Long createdId = recruitmentService.createRecruitment(createDto);
+        return ResponseEntity.ok(new RecruitmentCreateResponseDto(createdId));
+    }
+
+    @PatchMapping("/{recruitmentId}")
+    public ResponseEntity<Void> edit(
+            @PathVariable Long recruitmentId,
+            @RequestBody RecruitmentEditDto editDto
+            ) {
+        recruitmentService.editRecruitment(recruitmentId, editDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{recruitmentId}")
+    public ResponseEntity<Void> delete(
+            @PathVariable Long recruitmentId) {
+        recruitmentService.deleteRecruitment(recruitmentId);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<RecruitmentSearchResponseDto>> search(
+            @RequestParam String search,
+            @RequestParam int page,
+            @RequestParam int size
+    ) {
+        Page<RecruitmentSearchResponseDto> searchResponses = recruitmentService.searchRecruitment(search, PageRequest.of(page, size));
+        return ResponseEntity.ok(searchResponses);
+    }
+
+    @GetMapping("/{recruitmentId}")
+    public ResponseEntity<RecruitmentDetailResponseDto> getDetail(
+            @PathVariable Long recruitmentId) {
+        RecruitmentDetailResponseDto detailResponse = recruitmentService.getRecruitmentDetail(recruitmentId);
+        return ResponseEntity.ok(detailResponse);
+    }
+
+    @PostMapping("/{recruitmentId}")
+    public ResponseEntity<Void> apply(
+            @PathVariable Long recruitmentId,
+            @RequestBody RecruitmentApplyDto applyDto
+    ) {
+        recruitmentService.applyRecruitment(recruitmentId, applyDto.getMemberId());
+        return ResponseEntity.ok().build();
+    }
+
+}
---
+++ src/main/java/com/wjy/wantedpreonboarding/dto/res/RecruitmentCreateResponseDto.java
@@ -0,0 +1,10 @@
+package com.wjy.wantedpreonboarding.dto.res;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RecruitmentCreateResponseDto {
+    private Long created;
+}
