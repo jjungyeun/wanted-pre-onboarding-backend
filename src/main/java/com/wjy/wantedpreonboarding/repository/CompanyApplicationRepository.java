75571963079f6fb71064b98d7f580a9b41bf6e85
jjungyeun <wjyddd@naver.com>
2023-10-16 11:47:27 +0900

    feat #3 - 엔티티별 레포지토리 인터페이스 정의
    - JpaRepository를 상속하는 레포지토리 인터페이스 정의

---
+++ src/main/java/com/wjy/wantedpreonboarding/repository/CompanyApplicationRepository.java
@@ -0,0 +1,7 @@
+package com.wjy.wantedpreonboarding.repository;
+
+import com.wjy.wantedpreonboarding.entity.CompanyApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyApplicationRepository extends JpaRepository<CompanyApplication, Long> {
+}
---
+++ src/main/java/com/wjy/wantedpreonboarding/repository/RecruitmentRepository.java
@@ -0,0 +1,7 @@
+package com.wjy.wantedpreonboarding.repository;
+
+import com.wjy.wantedpreonboarding.entity.Recruitment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+}
